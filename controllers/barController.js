0f94bbd6859beeb0fcdcc1b55009a137ac712d75
yungwifi <smerryman@me.com>
2018-04-13 12:56:09 -0400

    CRUD set up for bars

--- controllers/barController.js
+++ controllers/barController.js
@@ -1,69 +1,70 @@
 const express = require('express')
 const router = express.Router({ mergeParams: true })
 
-const userModel = require("../models/user")
 const barModel = require("../models/bar")
+const userModel = require("../models/user")
+
+router.post('/', (req, res) => {
+    console.log(req.params.id)
+    const userId = req.params.id
+    const newBar = req.body
+
+    userModel.findById(userId)
+        .then((users) => {
+            const newBar = new barModel({
+                name: req.body.name,
+                location: req.body.location,
+                rating: req.body.rating
+            })
+
+            users.bars.push(newBar)
+            return users.save()
+        }).then((updatedUser) => {
+            res.redirect(`/users/${req.params.id}/`)
+        })
+})
+
+//GET New Bar 
+router.get('/new', (req, res) => {
+    const userId = req.params.id
+    userModel.findById(userId)
+    res.render('bars/new', {
+        userId
+    })
+})
+
+// DELETE Bar
+router.delete('/:barsId', (req, res) => {
+    const userId = req.params.id
+    userModel.findById(userId)
+        .then((user) => {
+            user.bars.id(req.params.barsId).remove()
+            return user.save()
+        }).then(() => {
+            res.redirect(`/users/${req.params.id}/`)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 //GET Specific Bar Page
 router.get('/:barsId', (req, res) => {
-
-    userModel.findById(req.params.userId)
+    console.log('SHOW ROUTE HIT')
+    const userId = req.params.id
+    userModel.findById(req.params.id)
         .then((users) => {
             console.log(users)
             const bar = users.bars.id(req.params.barsId)
             res.render('bars/details', {
                 userId,
-                bars
+                users,
+                bar
             })
         })
         .catch((err) => {
             console.log(err)
         })
 })
-
-// POST New Bar
-router.post('/', (req, res) => {
-    const userId = req.params.userId
-    console.log('Req body new bar: ', req.body)
-    const newBar = new barModel({
-        name: req.body.name,
-        location: req.body.location,
-        rating: req.body.rating
-    })
-    newBar.save()
-        .then((savedBar) => {
-            res.redirect(`/users/${userId}`)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-
-//GET New Bar 
-router.get('/new', (req, res) => {
-    res.render('bars/new')
-})
-
-// DELETE User
-router.delete('/:barsId', (req, res) => {
-    barModel.findByIdAndRemove(req.params.barId)
-        .then(() => {
-            res.redirect(`/users/${userId}`)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-router.delete('/:barsId', (req, res) => {
-    User.findById(req.params.userId)
-        .then((user) => {
-            const bar = user.bars.id(req.params.barsId)
-            bar.remove()
-            return user.save()
-        }).then(() => {
-            res.redirect(`/users/${req.params.userId}/bars`)
-        })
-})
-
 
 module.exports = router
